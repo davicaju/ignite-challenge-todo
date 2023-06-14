db28a73d3ec6c56af04e7e371e5c5fc015d9d451
davicaju <devcaju@gmail.com>
2023-06-13 21:47:45 -0300

    feat: add taskList component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { Header } from "./components/Header";
 import { InputGroup } from "./components/InputGroup";
+import { TaskList } from "./components/TaskList";
 
 export function App() {
   return (
     <>
       <Header />
       <InputGroup />
+      <TaskList />
     </>
   );
 }
---
+++ src/components/TaskList/index.tsx
@@ -0,0 +1,9 @@
+export function TaskList() {
+  return (
+    <ul>
+      <li>Learn typescript</li>
+      <li>Learn react</li>
+      <li>Make a coffee</li>
+    </ul>
+  );
+}
